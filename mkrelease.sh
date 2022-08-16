74b7547240a492e10d01261ae23b4e634fd772c7
Fantasy <fantasy@FantasydeAir.lan>
2022-08-16 19:47:09 +0800

    add a easy way to mkfw

--- mkrelease.sh
+++ mkrelease.sh
@@ -4,8 +4,8 @@
 #export PATH=~/mkfw:$PATH
 
 cd esplay-launcher
-ffmpeg -i main/gfxTile.png -f rawvideo -pix_fmt rgb565 main/gfxTile.raw -y
-cat main/gfxTile.raw | xxd -i > main/gfxTile.inc
+#ffmpeg -i main/gfxTile.png -f rawvideo -pix_fmt rgb565 main/gfxTile.raw -y
+#cat main/gfxTile.raw | xxd -i > main/gfxTile.inc
 #idf.py menuconfig
 idf.py build
 cd ../esplay-gnuboy
@@ -19,6 +19,6 @@ cd ../esplay-smsplusgx
 idf.py build
 cd ..
 
-../mkfw Retro-Emulation assets/tile.raw 0 16 1297504 launcher esplay-launcher/build/esplay-launcher.bin 0 17 642496 esplay-nofrendo esplay-nofrendo/build/esplay-nofrendo.bin 0 18 619056 esplay-gnuboy esplay-gnuboy/build/esplay-gnuboy.bin 0 19 1328848 esplay-smsplusgx esplay-smsplusgx/build/esplay-smsplusgx.bin
-rm esplay-retro-emu.fw
-mv firmware.fw esplay-retro-emu.fw
+#../mkfw Retro-Emulation assets/tile.raw 0 16 1297504 launcher esplay-launcher/build/esplay-launcher.bin 0 17 642496 esplay-nofrendo esplay-nofrendo/build/esplay-nofrendo.bin 0 18 619056 esplay-gnuboy esplay-gnuboy/build/esplay-gnuboy.bin 0 19 1328848 esplay-smsplusgx esplay-smsplusgx/build/esplay-smsplusgx.bin
+#rm esplay-retro-emu.fw
+#mv firmware.fw esplay-retro-emu.fw
